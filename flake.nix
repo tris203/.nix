f0f93613ecb313e26ccad122c06b286b9e55c0c4
tris203 <admin@snappeh.com>
2024-04-12 11:52:34 +0000

    feat: wsl

--- flake.nix
+++ flake.nix
@@ -8,6 +8,12 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixos-wsl = {
+url = "github:nix-community/NixOS-WSL";
+inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -26,5 +32,23 @@
          inputs.home-manager.nixosModules.default
       ];
     };
+
+    nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/wsl/configuration.nix
+        ./modules/nixos/awesome.nix
+        ./modules/nixos/discord.nix
+        ./modules/nixos/general.nix
+        ./modules/nixos/git.nix
+        ./modules/nixos/neovim.nix
+        ./modules/nixos/programming_langs.nix
+        ./modules/nixos/terminal_tools.nix
+        ./modules/nixos/zsh.nix
+         inputs.home-manager.nixosModules.default
+	 inputs.nixos-wsl.nixosModules.wsl
+      ];
+    };
+
   };
 }
