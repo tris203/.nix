a1614aeb28a61f481fa21cbe4eabb9a85d38da37
tris203 <admin@snappeh.com>
2024-04-12 22:43:01 +0100

    feat: picom

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -70,6 +70,8 @@
     #media-session.enable = true;
   };
 
+  services.picom.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
