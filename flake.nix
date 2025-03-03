a0276a55ca275702862922bd1d9339e7bdd6893f
tris203 <admin@snappeh.com>
2025-03-03 18:08:19 +0000

    feat: update flake configuration and remove shell.nix
    - Changed .envrc to use flake instead of nix
    - Updated flake.lock with new revisions and hashes
    - Added supported systems and devShells to flake.nix
    - Removed shell.nix as it's now redundant

--- flake.nix
+++ flake.nix
@@ -35,6 +35,11 @@
 
   outputs = { nixpkgs, ... }@inputs:
     let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+
       overlays = [
         inputs.neovim-nightly-overlay.overlays.default
         (import ./overlays/awesome-git.nix)
@@ -107,6 +112,16 @@
           inputs.nixos-wsl.nixosModules.wsl
         ];
       };
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ nixd nixpkgs-fmt deadnix ];
+          };
+        });
 
     };
 }
